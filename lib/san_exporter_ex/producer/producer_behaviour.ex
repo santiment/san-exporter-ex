3c0ce3684166c41b6b99f8935217e49d36adefff
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-05-31 11:11:17 +0300

    Remove the init/1 callback from the producer behaviour

--- lib/san_exporter_ex/producer/producer_behaviour.ex
+++ lib/san_exporter_ex/producer/producer_behaviour.ex
@@ -4,7 +4,6 @@ defmodule SanExporterEx.ProducerBehaviour do
 
   @type result :: :ok | {:error, String.t()}
 
-  @callback init(map | list) :: :ok
   @callback send_data(topic, data) :: result
   @callback send_data_async(topic, data) :: result
 end
